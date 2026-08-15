e7ff79b304dacf0ae484db98a989123ea3e79a91
0xveya <ghgstefan@gmail.com>
2026-08-15 03:24:44 +0200

    feat: added map helper methods and attached alloc to type

--- include/sigma/env.h
+++ include/sigma/env.h
@@ -15,6 +15,7 @@ typedef struct {
   map_entry_t *entries;
   usize len;
   usize cap;
+  allocator_t alloc;
 } str_map_t;
 
 bool sigma_env_init(str_map_t *env, allocator_t alloc, char **envp);
@@ -22,5 +23,5 @@ bool sigma_env_init(str_map_t *env, allocator_t alloc, char **envp);
 void str_map_deinit(str_map_t *map);
 str_t *str_map_get(str_map_t *map, str_t key);
 bool str_map_has(str_map_t *map, str_t key);
-bool str_map_init(str_map_t *map, usize capacity);
+bool str_map_init(str_map_t *map, allocator_t alloc, usize capacity);
 bool str_map_put(str_map_t *map, str_t key, str_t value);
--- src/env.c
+++ src/env.c
@@ -1,6 +1,8 @@
+#include <allocator_vtable.h>
 #include <qol.h>
 #include <sigma/env.h>
-#include <allocator_vtable.h>
+
+#include <string.h>
 
 static inline usize get_env_len(char **envp) {
   usize len = 0;
@@ -48,13 +50,8 @@ bool sigma_env_init(str_map_t *env, allocator_t alloc, char **envp) {
 
   usize env_count = get_env_len(envp);
 
-  env->entries = allocator_array(alloc, map_entry_t, env_count);
-
-  if (env_count != 0 && env->entries == NULL)
+  if (!str_map_init(env, alloc, env_count))
     return false;
-
-  env->len = 0;
-  env->cap = env_count;
 
   for (usize i = 0; i < env_count; i++) {
     str_t key;
@@ -63,12 +60,96 @@ bool sigma_env_init(str_map_t *env, allocator_t alloc, char **envp) {
     if (!parse_env_entry(envp[i], &key, &value))
       continue;
 
-    env->entries[env->len++] = (map_entry_t){
-        .key = key,
-        .value = value,
-        .occupied = true,
-    };
+    if (!str_map_put(env, key, value))
+      return false;
   }
 
   return true;
+}
+
+static bool str_eq(str_t a, str_t b) {
+  if (a.len != b.len)
+    return false;
+
+  return memcmp(a.items, b.items, a.len) == 0;
+}
+
+bool str_map_init(str_map_t *map, allocator_t alloc, usize capacity) {
+  if (map == NULL)
+    return false;
+
+  *map = (str_map_t){.alloc = alloc};
+  if (capacity == 0)
+    return true;
+
+  map->entries = allocator_array(map->alloc, map_entry_t, capacity);
+  if (map->entries == NULL)
+    return false;
+
+  map->cap = capacity;
+  return true;
+}
+
+static bool str_map_grow(str_map_t *map) {
+  if (map == NULL)
+    return false;
+
+  usize new_cap = map->cap == 0 ? 8 : map->cap * 2;
+  map_entry_t *new_entries = allocator_array(map->alloc, map_entry_t, new_cap);
+
+  if (new_entries == NULL)
+    return false;
+
+  if (map->entries != NULL && map->len > 0)
+    memcpy(new_entries, map->entries, map->len * sizeof(map_entry_t));
+
+  map->entries = new_entries;
+  map->cap = new_cap;
+  return true;
+}
+
+bool str_map_has(str_map_t *map, str_t key) {
+  return str_map_get(map, key) != NULL;
+}
+
+str_t *str_map_get(str_map_t *map, str_t key) {
+  if (map == NULL)
+    return NULL;
+
+  for (usize i = 0; i < map->len; i++) {
+    if (str_eq(map->entries[i].key, key))
+      return &map->entries[i].value;
+  }
+
+  return NULL;
+}
+
+bool str_map_put(str_map_t *map, str_t key, str_t value) {
+  if (map == NULL)
+    return false;
+
+  for (usize i = 0; i < map->len; i++) {
+    if (str_eq(map->entries[i].key, key)) {
+      map->entries[i].value = value;
+      return true;
+    }
+  }
+  if ((map->cap == 0 || (map->len + 1) * 100 > map->cap * 67) &&
+      !str_map_grow(map))
+    return false;
+
+  map->entries[map->len++] = (map_entry_t){
+      .key = key,
+      .value = value,
+      .occupied = true,
+  };
+
+  return true;
+}
+
+str_t str_from_cstr(char *s) {
+  return (str_t){
+      .items = s,
+      .len = strlen(s),
+  };
 }
